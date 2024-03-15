b94d8f2823ac3ed84490e49b1e49103c79471035
Nico-Grosso <nicogsr@gmail.com>
2024-03-15 20:39:13 -0300

    Agregué DTFecha pa practicar

---
+++ DTFecha.h
@@ -0,0 +1,17 @@
+#ifndef DTFECHA
+#define DTFECHA
+
+class DTFecha {
+    private:
+        int Dia, Mes, Ano;
+    public:
+        DTFecha(); // constructor por defecto
+        DTFecha(int dia, int mes, int ano); // constructor por parámetros
+        int getDia();
+        int getMes();
+        int getAno();
+        DTFecha setFecha(int dia, int mes, int ano); // devuelve un DTFecha pues no debe modificar el estado del datatype
+        ~DTFecha(); // destructor
+};
+
+#endif DTFECHA
