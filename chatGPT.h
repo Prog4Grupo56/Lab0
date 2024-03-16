71646e94c3fe9565b27328787d7dd4fa14d1184d
Nico-Grosso <nicogsr@gmail.com>
2024-03-16 18:07:29 -0300

    chatGPT header

---
+++ chatGPT.h
@@ -0,0 +1,21 @@
+#ifndef CHATGPT
+#define CHATGPT
+
+#include <iostream>
+
+
+class chatGPT{
+    private:
+        std::string Pregunta, Respuesta;
+    public:
+        chatGPT(); // constructor por defecto
+        chatGPT(std::string pregunta, std::string respuesta); // constructor por parámetros
+        std::string getPregunta();
+        std::string getRespuesta();
+        void setPregunta(std::string pregunta);
+        void setRespuesta(std::string respuesta);
+        std::string toString();
+        ~chatGPT(); // destructor
+};
+
+#endif CHATGPT
